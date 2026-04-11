4c93ba55fa4c64c925f803c1eba9be8e0a4d803f
temycodes <temycodes@gmail.com>
2026-04-11 08:34:37 +0100

    build: code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { ErrorBoundary } from "@/components/ui/ErrorBoundary";
 import { StripItApp } from "../components/StripItApp";
-
-
-
 export default function Home() {
   return (
     <ErrorBoundary>
